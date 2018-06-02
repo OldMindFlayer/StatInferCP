14a3e1d259f8693942833b468f7fdd756d1326fa
OldMindFlayer <OldMindFlayer@gmail.com>
2018-06-03 01:34:10 +0300

    adding Simulation Exercise

---
+++ base.R
@@ -0,0 +1,12 @@
+# setwd("C:/Workspace/Rscripts/Projects/CourseraLearning/StatInferCP")
+# source("base.R")
+library(R.utils)
+library(tidyverse)
+
+
+# fileUrl<-"https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
+# download.file(fileUrl, destfile = "StormData.csv.bz2")
+# dateDownloaded<-date()
+# bunzip2("StormData.csv.bz2", "StormData.csv", remove = FALSE, skip = TRUE)
+
+#data <- read_csv("StormData.csv")
